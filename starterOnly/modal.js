1aed0398270527f5b7d30c51f7889b00d6731b41
Rayan NAHI <rayan.nahi99@gmail.com>
2022-01-06 13:46:26 +0100

    add the feature to close modal Fix #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close") //Get the X element
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+modalClose.addEventListener("click",exitModal) // Add a click event listener on the X element which launch the exitModal function
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// Exit modal form
+
+function exitModal() {
+  modalbg.style.display = "none"; // Change the modalbg style on none
 }
 
 
